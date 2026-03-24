b12868278998678c29af6897c9c146cfef47cbba
Wzx03 <2779858404@qq.com>
2026-03-24 16:47:39 +0800

    修改了app.yaml -修改ai配置

---
+++ used-car-platform/src/main/java/com/wangzixian/usedcar/module/ai/service/impl/AiChatServiceImpl.java
@@ -0,0 +1,8 @@
+package com.wangzixian.usedcar.module.ai.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wangzixian.usedcar.module.ai.service.Aichatservice;
+public class AiChatServiceImpl extends ServiceImpl <SysChatMessageMapper, SysChatMessa>{
+    private final SysChatMessageMapper, SysChatMessa;
+    private final AiApiClient  aiApiClient;
+}
